cec37dae4e0f4f3f136c782726ce04e79de5c20c
Brian Galiwango <byanreynolds70@gmail.com>
2022-10-30 10:41:18 +0300

    update

--- project/test_hello.py
+++ project/test_hello.py
@@ -2,7 +2,7 @@ from hello import more_hello, more_goodbye
 
 
 def test_more_hello():
-    assert "Hello, world" == more_hello()
+    assert "Hello, world!" == more_hello()
 
 
 def test_more_goodbye():
